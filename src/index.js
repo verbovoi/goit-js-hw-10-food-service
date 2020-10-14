da78d4acffdbc3ed94401517c6d179e34fb59d90
Eugene Verbovoi <63497261+verbovoi@users.noreply.github.com>
2020-10-14 21:21:49 +0300

    Добавляет загрузку и изменение темы

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import menuItemsTemplate from "./menu-item-card.hbs";
 
 //Подключаем элементы DOM
 const menuListEl = document.querySelector(".js-menu");
+const switchThemeEl = document.querySelector('.theme-switch__control');
+const switchCheckboxEl = document.querySelector('#theme-switch-toggle');
+const bodyEl = document.querySelector('body');
+
+//Константа с перечислением тем
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 //Функция добавления разметки меню
 function createMenuItemsMarkup(items) {
@@ -11,3 +20,31 @@ function createMenuItemsMarkup(items) {
 }
 
 menuListEl.innerHTML = createMenuItemsMarkup(menuItems);
+
+//Функция изменения темы и запись в localStorage
+function changeMainTheme(item) {
+  if (item.target.checked) {
+    bodyEl.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    bodyEl.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+switchThemeEl.addEventListener('change', changeMainTheme);
+
+//Функция установки темы при загрузки страницы
+function setMainTheme() {
+  console.log(localStorage.getItem('theme'));
+
+  if (localStorage.getItem('theme') === Theme.LIGHT) {
+    bodyEl.classList.add(Theme.LIGHT);
+  } else {
+    bodyEl.classList.add(Theme.DARK);
+    switchCheckboxEl.checked = true;
+  }
+
+}
+
+setMainTheme();
